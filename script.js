76355047ba545b729e7ac9197e693f2bef21fe8d
Ryan Olson <ryanpolson3@gmail.com>
2023-05-20 23:23:44 -0500

    Updated carousel css and js

--- script.js
+++ script.js
@@ -24,22 +24,44 @@ for (i = 0; i < hamburgerMenuLinks.length; i++) {
 
 // Initialize Splide
 const splide = new Splide(".splide", {
-  type: "slide",
-  wheel: true,
+  type: "loop",
+  perPage: 1,
+  perMove: 1,
+  focus: 'left',
+  rewind: true,
+  wheel: false,
   pagination: false,
   gap: 15,
+  autoheight: true,
   breakpoints: {
-    500: {
+    767: {
       perPage: 1,
+      gap: 15,
+      width: '92%',
+      
     },
     768: {
       perPage: 2,
+      perMove: 2,
+      gap: 15,
     },
     1024: {
       perPage: 3,
+      perMove: 3,
+      gap: 15,
+      width: '92.5%',
     },
-    5500: {
+    1440: {
       perPage: 4,
+      perMove: 1,
+      gap: 15,
+      width: '94.5%',
     },
+    2560: {
+      perPage: 4,
+      perMove: 1,
+      width: '72%',
+      gap: 80,
+    }
   },
 }).mount();
